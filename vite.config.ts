e6e41afd515290b062118776c547bf9c8ad73f29
Sampleprovider(sp) <radioactivestuff.101@gmail.com>
2025-11-12 22:21:16 -0800

    oops it doesnt build

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-    build: {
-        target: 'modules',
-    },
     server: {
         port: 5180
     }
